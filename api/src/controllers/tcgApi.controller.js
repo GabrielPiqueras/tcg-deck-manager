42eaf598d05758e1b5be3f70eac5c32ca3a61fe8
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2022-10-28 01:41:38 +0200

    FUNCIONA PERO FALTA OPTIMIZAR

--- api/src/controllers/tcgApi.controller.js
+++ api/src/controllers/tcgApi.controller.js
@@ -2,6 +2,99 @@ import { getConnection } from '../database/database';
 import config from '../config';
 import fetch from 'node-fetch';
 
+const importTypes = async(req, res) => {
+    const connection = await getConnection();
+    const endpoint = 'https://api.pokemontcg.io/v2/types';
+
+    const request = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': config.tcg_api_key,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    const { data: types } = await request.json();
+
+    types.map((title) => {
+
+        // Insetando en la base de datos
+        connection.query('INSERT INTO types SET ?', { name: title }, function (error, results, fields) {
+            if (error) throw error;
+        });
+    });
+
+    res.json('Tipos elementales importados correctamente.');
+}
+
+const importSubtypes = async(req, res) => {
+    const connection = await getConnection();
+    const endpoint = 'https://api.pokemontcg.io/v2/subtypes';
+
+    const request = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': config.tcg_api_key,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    const { data: subtypes } = await request.json();
+
+    subtypes.map((title) => {
+
+        // Insetando en la base de datos
+        connection.query('INSERT INTO subtypes SET ?', { name: title }, function (error, results, fields) {
+            if (error) throw error;
+        });
+    });
+
+    res.json('Subtipos importados correctamente.');
+}
+
+const importSets = async(req, res) => {
+    const connection = await getConnection();
+    const endpoint = 'https://api.pokemontcg.io/v2/sets?orderBy=releaseDate';
+
+    const request = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': config.tcg_api_key,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    const { data: sets } = await request.json();
+    const today = new Date().toISOString().slice(0,10); // YYYY-MM-DD
+
+    sets.map((set) => {
+
+        let setData = {};
+
+        setData.shortening = set.id; // NOT NULL
+        setData.name = set.name // NOT NULL
+        setData.series = set.series || null;
+        setData.printed_total = set.printedTotal || 0;
+        setData.total = set.total || 0;
+        setData.ptc_go_code = set.ptcgoCode;
+        setData.release_date = set.releaseDate;
+        setData.symbol_url = set.images.symbol;
+        setData.logo_url = set.images.logo;
+        setData.created_at = today;
+        setData.updated_at = today;
+       
+        // Insetando en la base de datos
+        connection.query('INSERT INTO sets SET ?', setData, function (error, results, fields) {
+            if (error) throw error;
+        });
+    });
+
+    res.json('Sets importados correctamente.');
+}
+
 const importRatities = async(req, res) => {
     const connection = await getConnection();
     const endpoint = 'https://api.pokemontcg.io/v2/rarities';
@@ -28,9 +121,20 @@ const importRatities = async(req, res) => {
     res.json('Rarezas importadas correctamente.');
 }
 
-const importCards = async(req, res) => {
+const insertCardType = async(cardId, typeId) => {
+    // console.log(`Muestro typetext: ${typeText} y cardId: ${cardId}`);
+
     const connection = await getConnection();
-    const endpoint = 'https://api.pokemontcg.io/v2/cards';
+    connection.query('INSERT INTO card_types SET ?', { card_id: cardId, type_id: typeId}, function (error, results, fields) {
+        if (error) throw error;
+    });
+}
+
+const importCards = async(req, res) => {
+
+    const setId = req.params.setId; // set for expansion
+    const connection = await getConnection();
+    const endpoint = `https://api.pokemontcg.io/v2/cards?q=set.id:${setId}`;
 
     const request = await fetch(endpoint, {
         method: 'GET',
@@ -43,40 +147,85 @@ const importCards = async(req, res) => {
     
     const { data: cards } = await request.json();
 
-    cards.map((card) => {
+    cards.map(async (card) => {
 
-        let info = {};
+        let cardData = {};
 
-        info.id = card.id || null;
-        info.supertype = card.supertype || null;
-        info.name = card.name || null;
-        info.level = card.level || null;
-        info.setId = card.set.id || null;
-        info.evolvesFrom = card.evolvesFrom || null;
-        info.retreatCost = card.retreatCost || null;
-        info.convertedRetreatCost = card.convertedRetreatCost || null;
-        info.type1 = card.types[0] || null;
-        info.type2 = card.types[1] || null;
-        info.number = card.number || null;
-        info.artist = card.artist || null;
-        info.rarity = card.rarity || null;
-        info.flavorText = card.flavorText || null;
-        info.nationalPokedexNumbers = card.nationalPokedexNumbers[0] || null;
-        info.img_small = card.images.small || null;
-        info.img_large = card.images.large || null;
-        info.cardmarket = card.cardmarket.url || null;
+        // cardData.set_id = card.set.id  || null; // OJITOOOOOOOOOOOOOOOO
+
+        cardData.shortening = card.id;
+        cardData.supertype = card.supertype;
+        cardData.name = card.name;
+        cardData.number = card.number;
+        cardData.pokedex_number = card.nationalPokedexNumbers ? card.nationalPokedexNumbers[0] : null;
+        cardData.artist = card.artist || null;
+        cardData.hp = card.hp || null;
+        cardData.level = card.level || null;
+        cardData.evolves_from = card.evolvesFrom || null;
+        cardData.evolves_to = card.evolvesTo ? card.evolvesTo.toString() : null;
+        cardData.retried_cost = card.convertedRetreatCost || null;
+        cardData.retried_type = card.retreatCost ? card.retreatCost[0] : null;
+        cardData.small_img = card.images.small || null;
+        cardData.large_img = card.images.large || null;
+        cardData.cardmarket_url = card.cardmarket.url || null;  
         
+        // DATOS QUE OBTENER POR CONSULTA
+        // console.log('Muestro card: ', card);
+
         // Insetando en la base de datos
-        connection.query('INSERT INTO rarities SET ?', { name: info.name }, function (error, results, fields) {
-            if (error) throw error;
-            console.log('ID DE LA CARTA INSERTADA: ', results.insertId);
-        });
+
+        const sets = await connection.query(`SELECT id FROM sets WHERE shortening = '${card.set.id}'`);
+        const rarities = await connection.query(`SELECT id FROM rarities WHERE name = '${card.rarity}'`);
+        
+        cardData.set_id = sets[0].id;
+        cardData.rarity_id = rarities[0].id;
+        
+       
+        
+        // RECORRER TYPES
+
+        if (card.types) {
+            
+            card.types.map(async(type) => {
+                const results = await connection.query(`SELECT id FROM types WHERE name = '${type}'`);
+                const type_id = results[0].id || undefined;
+    
+                connection.query('INSERT INTO cards SET ?', cardData, function (error, results, fields) {
+                    if (error) throw error;
+        
+                    let card_id = results.insertId;
+                    
+                    insertCardType(card_id, type_id);
+                });
+            });
+        }
+              
+        // RECORRER SUBTYPES
+        // card.subtypes.map(async(subtype) => {
+
+        // });
+        // RECORRER ATTACKS
+        // card.attacks.map(async(attack) => {
+
+        // });
+        // RECORRER WEAKNESSES
+        // card.weakness.map(async() => {
+
+        // });
+        // RECORRER RESISTANCES
+        // card.resistances.map(async() => {
+
+        // });
+    
     });
     
-    res.json('Cartas importadas correctamente.');
+    res.json('Cartas importadas correctamente');
 }
 
 export const methods = {
     importCards,
-    importRatities
+    importRatities,
+    importTypes,
+    importSubtypes,
+    importSets
 }
--- api/src/routes/api.routes.js
+++ api/src/routes/api.routes.js
@@ -9,8 +9,11 @@ import { methods as tcgApiController } from '../controllers/tcgApi.controller';
 const router = Router();
 
 // Import routes
-router.get('/api/import-cards', tcgApiController.importCards);
+router.get('/api/import-cards/:setId', tcgApiController.importCards);
 router.get('/api/import-rarities', tcgApiController.importRatities);
+router.get('/api/import-types', tcgApiController.importTypes);
+router.get('/api/import-subtypes', tcgApiController.importSubtypes);
+router.get('/api/import-sets', tcgApiController.importSets);
 
 // Normal routes
 router.get('/api/cards', cardController.getCards);
