8970e7e401a12154608debfe73e335b3d60e27dc
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2022-11-16 00:38:50 +0100

    Importaciones correctas.

--- api/src/controllers/tcgApi.controller.js
+++ api/src/controllers/tcgApi.controller.js
@@ -2,14 +2,6 @@ import { getConnection } from '../database/database';
 import config from '../config';
 import fetch from 'node-fetch';
 
-const removeWithFilter = (array) => {
-    let outputArray = array.filter((v, i, self) => {
-        return i == self.indexOf(v);
-    });
-      
-    return outputArray;
-}
-
 const importTypes = async(req, res) => {
     const connection = await getConnection();
     const endpoint = 'https://api.pokemontcg.io/v2/types';
@@ -130,9 +122,8 @@ const importRatities = async(req, res) => {
 }
 
 const insertCardType = async(cardId, typeId) => {
-    // console.log(`Muestro typetext: ${typeText} y cardId: ${cardId}`);
-
     const connection = await getConnection();
+
     connection.query('INSERT INTO card_types SET ?', { card_id: cardId, type_id: typeId}, function (error, results, fields) {
         if (error) throw error;
     });
@@ -177,6 +168,34 @@ const insertCardAbility = async(cardId, name, text, type) => {
     });
 }
 
+const insertCardRule = async(cardId, textRule) => {
+
+    const connection = await getConnection();
+
+    const rule = {
+        card_id: cardId,
+        text: textRule,
+    };
+
+    connection.query('INSERT INTO card_rules SET ?', rule, function (error, results, fields) {
+        if (error) throw error;
+    });
+}
+
+const insertCardLegality = async(cardId, legalityId) => {
+
+    const connection = await getConnection();
+
+    const legality = {
+        card_id: cardId,
+        legality_id: legalityId,
+    };
+
+    connection.query('INSERT INTO card_legalities SET ?', legality, function (error, results, fields) {
+        if (error) throw error;
+    });
+}
+
 const insertCardAttack = async(cardId, attackObj) => {
 
     const connection = await getConnection();
@@ -210,10 +229,9 @@ const insertCardAttack = async(cardId, attackObj) => {
 }
 
 const insertAttackCost = async(attackId, uniqueType, cost) => {
-
     const connection = await getConnection();
-    
-    const query = await connection.query(`SELECT id, name FROM types WHERE name = '${uniqueType}'`);
+
+    const query = await connection.query(`SELECT id FROM types WHERE name = '${uniqueType}'`);
     const typeId = query[0].id || undefined;
     
     const attackCost = {
@@ -232,7 +250,8 @@ const importCards = async(req, res) => {
     const setId = req.params.setId; // set for expansion
     const connection = await getConnection();
     const endpoint = `https://api.pokemontcg.io/v2/cards?q=set.id:${setId}`;
-
+    // const endpoint = `https://api.pokemontcg.io/v2/cards?q=set.id:${setId} id:swsh11-217`;
+    
     const request = await fetch(endpoint, {
         method: 'GET',
         headers: {
@@ -247,9 +266,7 @@ const importCards = async(req, res) => {
     cards.map(async (card) => {
 
         let cardData = {};
-
-        console.log('Card name: ,', card.name);
-
+        
         cardData.shortening = card.id;
         cardData.supertype = card.supertype;
         cardData.name = card.name;
@@ -274,17 +291,18 @@ const importCards = async(req, res) => {
         const sets = await connection.query(`SELECT id FROM sets WHERE shortening = '${card.set.id}'`);
         const rarities = await connection.query(`SELECT id FROM rarities WHERE name = '${card.rarity}'`);
         
+
         cardData.set_id = sets[0].id;
         cardData.rarity_id = rarities[0].id;
-        
+
         let card_id;
 
         // IMPORTO LA CARTA Y OBTENGO SU ID
+
         connection.query('INSERT INTO cards SET ?', cardData, function (error, results, fields) {
             if (error) throw error;
 
             card_id = results.insertId;
-            console.log('Muestro cardId dentro: ', card_id);
             
             // RECORRO SUS TIPOS (SI TUVIERA)
             if (card.types) {
@@ -292,22 +310,22 @@ const importCards = async(req, res) => {
                 card.types.map(async(type) => {
                     const query = await connection.query(`SELECT id FROM types WHERE name = '${type}'`);
                     const type_id = query[0].id || undefined;
-                    
+
                     insertCardType(card_id, type_id);
                 });
             }
-                  
+              
             // RECORRER SUBTYPES (si tuviera)
             if (card.subtypes) {
                 
                 card.subtypes.map(async(subtype) => {
                     const query = await connection.query(`SELECT id FROM subtypes WHERE name = '${subtype}'`);
                     const subtype_id = query[0].id || undefined;
-                    
+
                     insertCardSubtype(card_id, subtype_id);
                 });
             }
-          
+
             // RECORRER ATTACKS
             if (card.attacks) {
                 
@@ -316,20 +334,20 @@ const importCards = async(req, res) => {
                     insertCardAttack(card_id, attackObj);
                 });
             }
-    
+
             // RECORRER WEAKNESSES
             if (card.weaknesses) {
     
                 card.weaknesses.map(async(weakness) => {
                     const query = await connection.query(`SELECT id FROM types WHERE name = '${weakness.type}'`);
                     const type_id = query[0].id || undefined;
-    
+                    
                     const w_value = parseInt(weakness.value.replace(/[^\-?(0-9)]+/g, ''));
                     insertCardEfficience(card_id, type_id, w_value);
                 });
             }
-    
-            // RECORRER RESISTANCES
+
+            // // RECORRER RESISTANCES
             if (card.resistances) {
     
                 card.resistances.map(async(resistance) => {
@@ -340,26 +358,43 @@ const importCards = async(req, res) => {
                     insertCardEfficience(card_id, type_id, r_value);
                 });
             }
-    
+
             if (card.abilities) {
-    
-                console.log('Entra 2');
-                console.log('card_id: ', card_id);
     
                 card.abilities.map(async(ability) => {
     
                     const name = ability.name;
                     const text = ability.text;
                     const type = ability.type;
+                    
+                    insertCardAbility(card_id, name, text, type);
+                });
+            }
+
+            if (card.rules) {
     
-                    console.log('card_id, name, text, type: ', card_id, name, text, type);
+                card.rules.map(async(textRule) => {
+                    insertCardRule(card_id, textRule);
+                });
+            }
+
+            if (card.legalities) {
+
+                Object.entries(card.legalities).forEach(async([legality, value]) => {
+                    
+
+                    const query = await connection.query(`SELECT id FROM legalities WHERE name = '${legality}'`);
+                    const legality_id = query[0].id || undefined;
+
+                    // console.log('card id + leg: ', card.name, card_id, legality_id);
+
+                    insertCardLegality(card_id, legality_id);
                 });
             }
         });
-
     });
     
-    res.json('Cartas importadas correctamentee');
+    res.json('Cartas importadas correctamente.');
 }
 
 export const methods = {
